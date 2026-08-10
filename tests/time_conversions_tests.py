71776649a6a51cd5583f098145ff68959ad2dc15
Enrique Condes <kosmerebel@gmail.com>
2026-08-10 10:52:16 +0800

    Unify test style. Define and  use self.func in all tests

--- tests/time_conversions_tests.py
+++ tests/time_conversions_tests.py
@@ -8,39 +8,45 @@ DEVELOPMENT = False
 
 @unittest.skipIf(DEVELOPMENT, "Development")
 class Test_gps2mjd(unittest.TestCase):
+    def setUp(self):
+        self.func = gps2mjd
+
+    def tearDown(self):
+        self.func = None
+
     def test_input_types(self):
         # Basic types that must be rejected
-        self.assertRaises(TypeError, gps2mjd, '0')
-        self.assertRaises(TypeError, gps2mjd, 0)
-        self.assertRaises(TypeError, gps2mjd, True)
-        self.assertRaises(TypeError, tdt2tdb, object())
-        self.assertRaises(TypeError, tdt2tdb, None)
-        self.assertRaises(ValueError, gps2mjd, ['a', 'b'])
+        self.assertRaises(TypeError, self.func, '0')
+        self.assertRaises(TypeError, self.func, 0)
+        self.assertRaises(TypeError, self.func, True)
+        self.assertRaises(TypeError, self.func, object())
+        self.assertRaises(TypeError, self.func, None)
+        self.assertRaises(ValueError, self.func, ['a', 'b'])
 
         # Check expected input types do not raise an exception
         try:
-            gps2mjd(0.0)  # float
-            gps2mjd([0.0])  # list
-            gps2mjd((0.0, 1.0))  # tuple
-            gps2mjd(np.asarray(0.0))  # numpy array-like
-        except ValueError:
+            self.func(0.0)  # float
+            self.func([0.0])  # list
+            self.func((0.0, 1.0))  # tuple
+            self.func(np.asarray(0.0))  # numpy array-like
+        except TypeError:
             self.fail()
 
     def test_output_type(self):
-        self.assertIsInstance(gps2mjd(0.0), np.ndarray,
+        self.assertIsInstance(self.func(0.0), np.ndarray,
                               "Failure to produce correct output type from a float value")
-        self.assertIsInstance(gps2mjd([0.0]), np.ndarray,
+        self.assertIsInstance(self.func([0.0]), np.ndarray,
                               "Failure to produce correct output type from list")
-        self.assertIsInstance(gps2mjd((0.0, 1.0)), np.ndarray,
+        self.assertIsInstance(self.func((0.0, 1.0)), np.ndarray,
                               "Failure to produce correct output type from tuple")
-        self.assertIsInstance(gps2mjd(np.asarray(0.0)), np.ndarray,
+        self.assertIsInstance(self.func(np.asarray(0.0)), np.ndarray,
                               "Failure to produce correct output type from an array-like value")
 
     def test_output_value(self):
         # MJD at GPS epoch (6-Jan-1980 00:00:00)
-        self.assertAlmostEqual(gps2mjd(0.0), 44244.0)
+        self.assertAlmostEqual(self.func(0.0), 44244.0)
         # MJD at GPS epoch (8-Jul-2026 04:48:36)
-        self.assertAlmostEqual(gps2mjd(1467521334.0), 61229.20041667)
+        self.assertAlmostEqual(self.func(1467521334.0), 61229.20041667)
 
 
 @unittest.skipIf(DEVELOPMENT, "Development")
@@ -49,130 +55,146 @@ class Test_leapseconds(unittest.TestCase):
     MAX_VALUE = 57754.0
     RANGE = MAX_VALUE - MIN_VALUE
 
+    def setUp(self):
+        self.func = leap_seconds
+
+    def tearDown(self):
+        self.func = None
+
     def test_input_types(self):
         # Basic types that must be rejected
-        self.assertRaises(TypeError, leap_seconds, '0')
-        self.assertRaises(TypeError, leap_seconds, 41317)
-        self.assertRaises(TypeError, leap_seconds, True)
-        self.assertRaises(TypeError, leap_seconds, object())
-        self.assertRaises(TypeError, leap_seconds, None)
-        self.assertRaises(ValueError, leap_seconds, ['a', 'b'])
+        self.assertRaises(TypeError, self.func, '0')
+        self.assertRaises(TypeError, self.func, 41317)
+        self.assertRaises(TypeError, self.func, True)
+        self.assertRaises(TypeError, self.func, object())
+        self.assertRaises(TypeError, self.func, None)
+        self.assertRaises(ValueError, self.func, ['a', 'b'])
 
         # Check expected input types do not raise an exception
         try:
-            leap_seconds(self.MIN_VALUE)  # float
-            leap_seconds([self.MIN_VALUE])  # list
-            leap_seconds((self.MIN_VALUE, self.MIN_VALUE))  # tuple
-            leap_seconds(np.asarray(self.MIN_VALUE))  # numpy array-like
-        except ValueError:
+            self.func(self.MIN_VALUE)  # float
+            self.func([self.MIN_VALUE])  # list
+            self.func((self.MIN_VALUE, self.MIN_VALUE))  # tuple
+            self.func(np.asarray(self.MIN_VALUE))  # numpy array-like
+        except TypeError:
             self.fail()
 
     def test_output_types(self):
-        self.assertIsInstance(leap_seconds(self.MIN_VALUE),
-                              (int, float, np.ndarray))
-        self.assertIsInstance(leap_seconds([self.MIN_VALUE]),
-                              (int, float, np.ndarray))
-        self.assertIsInstance(leap_seconds((self.MIN_VALUE, self.MIN_VALUE)),
-                              (int, float, np.ndarray))
-        self.assertIsInstance(leap_seconds(np.asarray(self.MIN_VALUE)),
-                              (int, float, np.ndarray))
+        self.assertIsInstance(self.func(self.MIN_VALUE), float)
+        self.assertIsInstance(self.func([self.MIN_VALUE]), np.ndarray)
+        self.assertIsInstance(self.func(
+            (self.MIN_VALUE, self.MIN_VALUE)), np.ndarray)
+        self.assertIsInstance(self.func(np.asarray(self.MIN_VALUE)), float)
 
     def test_input_values(self):
         # Random value below the first defined value
-        self.assertRaises(ValueError, leap_seconds,
+        self.assertRaises(ValueError, self.func,
                           random.random() * self.MIN_VALUE)
 
         # Check that values in range do not throw an exception
         try:
             # Generate a random value inside the defined range
             randVal = (random.random() * self.RANGE) + self.MIN_VALUE
-            leap_seconds(randVal)
+            self.func(randVal)
         except ValueError:
             self.fail()
 
-        # Check that values betyond the range do not throw an exception
+        # Check that values beyond the range do not throw an exception
         try:
             # Generate a random value inside the defined range
             randVal = (random.random() * self.RANGE) + self.MAX_VALUE + 1
-            leap_seconds(randVal)
+            self.func(randVal)
         except ValueError:
             self.fail()
 
     def test_output_values(self):
-        self.assertEqual(leap_seconds(self.MIN_VALUE), 10)
+        self.assertEqual(self.func(self.MIN_VALUE), 10)
         self.assertListEqual(
-            list(leap_seconds([self.MIN_VALUE, self.MAX_VALUE])), [10.0, 37.0])
+            list(self.func([self.MIN_VALUE, self.MAX_VALUE])), [10.0, 37.0])
 
         # Generate a random value inside the defined range
         randVal = (random.random() * self.RANGE) + self.MIN_VALUE
-        res = leap_seconds(randVal)
+        res = self.func(randVal)
         self.assertGreaterEqual(res, 10.0)
         self.assertLessEqual(res, 37.0)
 
         # Generate a random value greater than the max defined value
         randVal = (random.random() * self.RANGE) + self.MAX_VALUE + 1
-        self.assertEqual(leap_seconds(randVal), 37.0)
+        self.assertEqual(self.func(randVal), 37.0)
 
 
 @unittest.skipIf(DEVELOPMENT, "Development")
 class Test_tdt2tdb(unittest.TestCase):
+    def setUp(self):
+        self.func = tdt2tdb
+
+    def tearDown(self):
+        self.func = None
+
     def test_input_types(self):
-        self.assertRaises(TypeError, tdt2tdb, 'a')
-        self.assertRaises(TypeError, tdt2tdb, True)
-        self.assertRaises(TypeError, tdt2tdb, [12])
-        self.assertRaises(TypeError, tdt2tdb, (12, 13))
-        self.assertRaises(TypeError, tdt2tdb, object())
-        self.assertRaises(TypeError, tdt2tdb, None)
+        self.assertRaises(TypeError, self.func, 'a')
+        self.assertRaises(TypeError, self.func, True)
+        self.assertRaises(TypeError, self.func, [12])
+        self.assertRaises(TypeError, self.func, (12, 13))
+        self.assertRaises(TypeError, self.func, object())
+        self.assertRaises(TypeError, self.func, None)
 
         # Check expected input types do not raise an exception
         try:
-            tdt2tdb(123)
-            tdt2tdb(123.0)
-            tdt2tdb(np.asarray([123], dtype=float))
-        except ValueError:
+            self.func(123)
+            self.func(123.0)
+            self.func(np.asarray([123], dtype=float))
+        except TypeError:
             self.fail()
 
     def test_output_types(self):
-        self.assertIsInstance(tdt2tdb(123), float)
-        self.assertIsInstance(tdt2tdb(123.0), float)
-        self.assertIsInstance(tdt2tdb(np.asarray([123.0, 123.0])), np.ndarray)
+        self.assertIsInstance(self.func(123), float)
+        self.assertIsInstance(self.func(123.0), float)
+        self.assertIsInstance(
+            self.func(np.asarray([123.0, 123.0])), np.ndarray)
 
     def test_input_values(self):
         # Check that the input supports both negative and positive values
         try:
-            tdt2tdb(0)
-            tdt2tdb(475.0)
-            tdt2tdb(-10.0)
+            self.func(0)
+            self.func(475.0)
+            self.func(-10.0)
         except ValueError:
             self.fail()
 
     def test_output_values(self):
-        self.assertAlmostEqual(tdt2tdb(0), -0.001181, 6)
-        self.assertAlmostEqual(tdt2tdb(475.0), 0.001494, 6)
-        self.assertAlmostEqual(tdt2tdb(-10.0), -0.001365, 6)
+        self.assertAlmostEqual(self.func(0), -0.001181, 6)
+        self.assertAlmostEqual(self.func(475.0), 0.001494, 6)
+        self.assertAlmostEqual(self.func(-10.0), -0.001365, 6)
 
 
 @unittest.skipIf(DEVELOPMENT, "Development")
 class Test_gmst(unittest.TestCase):
+    def setUp(self):
+        self.func = gmst
+
+    def tearDown(self):
+        self.func = None
+
     def test_input_types(self):
-        self.assertRaises(ValueError, gmst, 'a')
-        self.assertRaises(ValueError, gmst, True)
-        self.assertRaises(ValueError, gmst, 1)
-        self.assertRaises(ValueError, gmst, (12.0, 13.0))
-        self.assertRaises(ValueError, gmst, [12.0])
-        self.assertRaises(ValueError, gmst, object())
-        self.assertRaises(ValueError, gmst, None)
+        self.assertRaises(ValueError, self.func, 'a')
+        self.assertRaises(ValueError, self.func, True)
+        self.assertRaises(ValueError, self.func, 1)
+        self.assertRaises(ValueError, self.func, (12.0, 13.0))
+        self.assertRaises(ValueError, self.func, [12.0])
+        self.assertRaises(ValueError, self.func, object())
+        self.assertRaises(ValueError, self.func, None)
 
         # Check expected input types do not raise an exception
         try:
-            gmst(123.0)
-            gmst(np.asarray([123], dtype=float))
+            self.func(123.0)
+            self.func(np.asarray([123], dtype=float))
         except ValueError:
             self.fail()
 
     def test_output_types(self):
-        self.assertIsInstance(gmst(12.0), float)
-        self.assertIsInstance(gmst(np.asarray([12.0, 13.0])), np.ndarray)
+        self.assertIsInstance(self.func(12.0), float)
+        self.assertIsInstance(self.func(np.asarray([12.0, 13.0])), np.ndarray)
 
     def test_output_values(self):
         SCALE = 100
@@ -181,7 +203,7 @@ class Test_gmst(unittest.TestCase):
         for i in range(10):
             a = random.random() * SCALE
             lst.append(a)
-            st = gmst(a)
+            st = self.func(a)
             self.assertGreaterEqual(st, 0)
             self.assertLessEqual(st, 24)
 
@@ -189,7 +211,7 @@ class Test_gmst(unittest.TestCase):
         for i in range(10):
             a = random.random() * SCALE * -1
             lst.append(a)
-            st = gmst(a)
+            st = self.func(a)
             self.assertGreaterEqual(st, 0)
             self.assertLessEqual(st, 24)
 
@@ -197,11 +219,12 @@ class Test_gmst(unittest.TestCase):
         for i in range(10):
             a = random.random() * SCALE * SCALE * SCALE
             lst.append(a)
-            st = gmst(a)
+            st = self.func(a)
             self.assertGreaterEqual(st, 0)
             self.assertLessEqual(st, 24)
 
-        st = gmst(np.asarray(lst))
+        # Check mixed range values in an ndarray
+        st = self.func(np.asarray(lst))
         if np.any(st < 0) or np.any(st > 24):
             self.fail()
 
